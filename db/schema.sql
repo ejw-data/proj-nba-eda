02a937d4f5f603f3fd69fcf2db22b82cecdbaf77
Erin Wills <ejw.data@gmail.com>
2023-03-05 19:49:51 -0600

    setting up mongodb and api

--- db/schema.sql
+++ db/schema.sql
@@ -1,37 +1,37 @@
 CREATE TABLE event_log(
-	event_id INT SERIAL PK,
+	event_id INT,
 	raw_description VARCHAR,
-	table VARCHAR,
+	table_name VARCHAR,
 	quarter VARCHAR,
 	game_time VARCHAR
-)
+);
 
 CREATE TABLE player(
-	player_id INT SERIAL PK,
+	player_id INT,
 	player_full_name VARCHAR,
 	first_name VARCHAR,
 	last_name VARCHAR,
 	birthdate DATE,
 	first_game_date DATE,
 	retirement_date DATE
-)
+);
 
 CREATE TABLE player_injury(
 	player_id INT,
 	injury VARCHAR,
 	start_date DATE,
 	end_date DATE
-)
+);
 
 CREATE TABLE team(
-	team_id INT SERIAL PK,
+	team_id INT,
 	team_name VARCHAR,
 	team_abbrev VARCHAR,
 	team_city VARCHAR,
 	team_mascot VARCHAR,
 	start_date VARCHAR,
 	end_date VARCHAR
-)
+);
 
 CREATE TABLE schedule(
 	game_id INT,
@@ -40,9 +40,9 @@ CREATE TABLE schedule(
 	location VARCHAR,
 	arena VARCHAR,
 	start_date DATE,
-	start_time_et DATETIME,
-	start_time_local DATETIME
-)
+	start_time_et DATE,
+	start_time_local DATE
+);
 
 CREATE TABLE player_team(
 	player_id INT,
@@ -50,24 +50,24 @@ CREATE TABLE player_team(
 	position VARCHAR,
 	start_date DATE,
 	end_date DATE
-)
+);
 
 CREATE TABLE play_by_play(
-	event_id INT PK,
+	event_id INT,
 	game_id INT,
 	event_description VARCHAR,
 	game_time VARCHAR,
 	player_id INT
-)
+);
 
-CREATE TABLE players-substitution(
+CREATE TABLE players_substitution(
 	player_id INT,
 	game_id INT,
 	team_id INT,
 	quarter VARCHAR,
 	game_time VARCHAR,
-	type VARHAR
-)
+	type VARCHAR
+);
 
 CREATE TABLE players_scored(
 	player_id INT,
@@ -77,7 +77,7 @@ CREATE TABLE players_scored(
 	quarter VARCHAR,
 	game_time VARCHAR,
 	shotclock INT
-)
+);
 
 CREATE TABLE players_fouls(
 	player_id INT,
@@ -85,7 +85,7 @@ CREATE TABLE players_fouls(
 	player_fouled VARCHAR,
 	shooting_foul VARCHAR,
 	points_scored INT
-)
+);
 
 CREATE TABLE players_defense(
 	player_id INT,
@@ -93,29 +93,29 @@ CREATE TABLE players_defense(
 	offesnse_player VARCHAR,
 	quarter VARCHAR,
 	game_time VARCHAR
-)
+);
 
 CREATE TABLE players_assists(
 	player_id INT,
 	type VARCHAR,
 	player_scored VARCHAR
-)
+);
 
 CREATE TABLE players_rebounds(
 	player_id INT,
 	type VARCHAR,
 	quarter VARCHAR,
-	game_time VARHCAR,
+	game_time VARCHAR,
 	next_play INT
-)
+);
 
 CREATE TABLE player_salary(
 	player_id INT,
 	season_start_year INT,
 	income_source VARCHAR,
 	conditions VARCHAR
-)
+);
 
 CREATE TABLE team_caps(
 	team_id INT
-)
+);
